35afb7908e81afca4f343595d14a74659702fbd3
Kyle Kurzhal <kkurzhal@gmail.com>
2016-02-06 15:05:29 -0600

    Adding bolt prefab with manual material applied for shader.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rgbd = GetComponent<Rigidbody>();
+        rgbd.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
