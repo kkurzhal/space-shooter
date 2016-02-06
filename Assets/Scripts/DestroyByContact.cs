5a31063a2c4bf3714b5c2723c7751ea8d0675b35
Kyle Kurzhal <kkurzhal@gmail.com>
2016-02-06 16:43:15 -0600

    Adding a rotating asteroid that can be shot.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Boundary")
+        {
+            Debug.Log("tag of other: " + other.tag);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+
+	void Start () {
+        Rigidbody rgbd = GetComponent<Rigidbody>();
+        rgbd.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
