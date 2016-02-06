44178962446f494c56208330d825c40d2f7cdeda
Kyle Kurzhal <kkurzhal@gmail.com>
2016-02-06 14:44:59 -0600

    Adding player movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
+	void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Rigidbody rgbd = GetComponent<Rigidbody>();
+        rgbd.velocity = movement * speed;
+        rgbd.position = new Vector3(
+            Mathf.Clamp(rgbd.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rgbd.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rgbd.rotation = Quaternion.Euler(0.0f, 0.0f, rgbd.velocity.x * -tilt);
+    }
+}
